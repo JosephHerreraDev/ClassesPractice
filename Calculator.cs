9f6280d6799b27d6a6ebecafd67a7603085d53fb
Joseph Herrera <josephherreraleon@outlook.com>
2020-07-07 18:52:45 -0500

    Project Loaded

---
+++ Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesPratice
+{
+	class Calculator
+	{
+		//fields
+		private string name;
+		private DateTime birthday;
+		private DayOfWeek birthdayDay;
+
+		//properties
+		public string Name
+		{
+			get { return this.name; }
+			set { this.name = value; }
+		}
+
+		public DateTime Birthday
+		{
+			get { return birthday; }
+			set { birthday = value; }
+		}
+
+		//methods
+		public void Dayofbirth()
+		{
+
+			birthdayDay =  birthday.Date.DayOfWeek;
+
+			Console.WriteLine("Hi {0}, your birthday was on a {1}", this.name, birthdayDay);
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesPratice
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Calculator calculator = new Calculator();
+
+			Console.WriteLine("Enter your name and the date of your " +
+				"birthday to receive the day of your birth");
+
+			Console.Write("Name: ");
+			calculator.Name = Console.ReadLine();
+
+			Console.Write("Birthday: ");
+			calculator.Birthday = Convert.ToDateTime(Console.ReadLine());
+
+			calculator.Dayofbirth();
+		}
+	}
+}
